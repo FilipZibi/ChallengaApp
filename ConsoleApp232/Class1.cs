f11bebf50fc713ef7f4faa4fa614190edfc5b432
Filip Zienkiewicz <filip.zienkiewicz1987@outlook.com>
2024-04-30 20:02:01 +0200

    drugi commit z Class1

---
+++ ConsoleApp232/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace Console123;
+
+public class User
+{
+
+    private List<int> score = new List<int>();
+
+    public User(string name, string surname, string Age)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Age = Age;       
+                
+    }
+
+    public string Name { get; private set; }
+
+    public string Surname { get; private set; }
+
+    public string Age { get; private set; }
+
+    public string Result 
+    { 
+        get
+        {
+            return 0;
+        }
+    }
+    public void AddScore(int number)
+    {
+        this.score.Add(number);
+    }
+}
+
+
--- ConsoleApp232/Program.cs
+++ ConsoleApp232/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Console123;
+User user1 = new User("Adam", "Mleko", "33");
+User user2 = new User("Monika", "Maslo", "35");
+User user3 = new User("Zuzia", "Dzem", "37");
+
+
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(3);
+user1.AddScore(4);
+user1.AddScore(6);
+
+user2.AddScore(1);
+user2.AddScore(2);
+user2.AddScore(3);
+user2.AddScore(4);
+user2.AddScore(5);
+
+user3.AddScore(10);
+user3.AddScore(9);
+user3.AddScore(8);
+user3.AddScore(7);
+user3.AddScore(6);
+
+Console.WriteLine("Employee1 result is", user1.Result);
+Console.WriteLine("Employee2 result is" , user2.Result);
+Console.WriteLine("Employee3 result is", user3.Result);
+
+if
+{
+    user1.Result > user2.Result && user1.Result > user3.Result
+        Console.WriteLine(user1.Result);
+
+}
+
+List<User> users = new List<User>();
+{
+    user1, user2, user3
+};
+
+int maxResult = -1;
+User userWithMaxResult = null;
+var result = user1.Result;
+
+foreach (var user in users)
+{
+    if (user.Result > maxResult)
+    {
+        userWithMaxResult = user;
+
+    }
+
+}
+
+Console.WriteLine(userWithMaxResult);
